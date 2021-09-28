30200f2fd4fc904e8daade5696c6f6f046255d16
libbyleonard22 <815989@mystma.org>
2021-09-28 14:43:27 -0500

    3

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 #WRITE YOUR CODE IN THIS FILE
 def password(p):
     
-    if p == "knights00":
-        return "ACCESS DENIED"
-
-    elif p == "knights19":
+    if p == "Knights19":
         return "ACCESS GRANTED"
 
     else:
-        return "tgis is weird"
+        return "ACCESS DENIED"
+
 print(password("p"))
