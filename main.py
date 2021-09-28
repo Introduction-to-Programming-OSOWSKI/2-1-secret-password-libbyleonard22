cff358a1d671bb8ce23431630dd34899feb9aef4
libbyleonard22 <815989@mystma.org>
2021-09-28 14:36:49 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #WRITE YOUR CODE IN THIS FILE
+def password(p):
+    
+    if p == "knights00":
+        return "ACCESS DENIED"
+
+    if p == "knights19":
+        return "ACCESS GRANTED"
+
+print(password("p"))
